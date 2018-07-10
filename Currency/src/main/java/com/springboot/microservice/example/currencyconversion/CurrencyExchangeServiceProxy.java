a4f189cf85bcc7de1a7804a43a8a88d6a9c70e7e
naveen <kumar.naveen@daffodilsw.com>
2018-07-10 14:58:17 +0530

    Now robin added eureka started

--- Currency/src/main/java/com/springboot/microservice/example/currencyconversion/CurrencyExchangeServiceProxy.java
+++ Currency/src/main/java/com/springboot/microservice/example/currencyconversion/CurrencyExchangeServiceProxy.java
@@ -1,15 +1,24 @@
 package com.springboot.microservice.example.currencyconversion;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 /*
-@FeignClient(name="forex-service" url="localhost:8100") - Declares that this is a Feign Client and the url at which forex-service is present is localhost:8100
+@FeignClient(name="forex-service" url="localhost:8000") - Declares that this is a Feign Client and the url at which forex-service is present is localhost:8100
 @GetMapping("/currency-exchange/from/{from}/to/{to}") - URI of the service we would want to consume
  */
-@FeignClient(name="forex-service",url="localhost:8000")
+//@FeignClient(name="forex-service",url="localhost:8000")
+//public interface CurrencyExchangeServiceProxy {
+//  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+//  public CurrencyConversionBean retrieveExchangeValue
+//    (@PathVariable("from") String from, @PathVariable("to") String to);
+//}
+
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
-  @GetMapping("/currency-exchange/from/{from}/to/{to}")
-  public CurrencyConversionBean retrieveExchangeValue
-    (@PathVariable("from") String from, @PathVariable("to") String to);
+@GetMapping("/currency-exchange/from/{from}/to/{to}")
+public CurrencyConversionBean retrieveExchangeValue
+(@PathVariable("from") String from, @PathVariable("to") String to);
 }
